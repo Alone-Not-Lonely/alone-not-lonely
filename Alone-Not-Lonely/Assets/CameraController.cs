4c5377b0ca471ea05cba51219f34f7bb97887a12
santoast <solsanto@ucsc.edu>
2020-11-06 12:12:22 -0800

    added a camera movement relative to mouse
    we don't have player movement in the direction of camera yet tho!

---
+++ Alone-Not-Lonely/Assets/CameraController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // movement constraints
+    public float minimumX = -60f;
+    public float maximumX = 60f;
+    public float minimumY = -306f;
+    public float maximum = 360f;
+
+    // sensitivity controls
+    public float sensitivityX = 15f;
+    public float sensitivityY = 15f;
+
+    // camera reference
+    public Camera cam;
+
+    // current roation
+    float rotationX = 0f;
+    float rotationY = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // update current values
+        rotationY += Input.GetAxis("Mouse X") * sensitivityX;
+        rotationX += Input.GetAxis("Mouse Y") * sensitivityY;
+
+        // constrain x
+        rotationX = Mathf.Clamp(rotationX, minimumX, maximumX);
+
+        // rotate game objects accordingly
+        transform.localEulerAngles = new Vector3(-rotationX, rotationY, 0);
+    }
+}
