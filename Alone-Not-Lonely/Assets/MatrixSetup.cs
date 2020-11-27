def50e537afeb9ae4300b00b640a068b952c077d
Rachel Haub <rachelhaub@gmail.com>
2020-11-26 17:58:56 -0800

    Added animted paint shader

---
+++ Alone-Not-Lonely/Assets/MatrixSetup.cs
@@ -0,0 +1,21 @@
+﻿ using System;
+ using UnityEngine;
+ [ExecuteInEditMode]
+ public class MatrixSetup : MonoBehaviour
+ {
+     private void OnEnable ()
+     {
+         Camera.onPreCull += SetupMatrices;
+     }
+ 
+     private void OnDisable ()
+     {
+         Camera.onPreCull -= SetupMatrices;
+     }
+ 
+     private void SetupMatrices (Camera cam)
+     {
+         //Just make sure you have a custom matrix property on your material called _InvProjectionMatrix and use that instead
+         Shader.SetGlobalMatrix ("_InvProjectionMatrix", cam.projectionMatrix.inverse);
+     }
+ }
