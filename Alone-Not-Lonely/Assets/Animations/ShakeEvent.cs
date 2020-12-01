cee2efe7809950b20cdb64deac866bd4af8296de
rbaua <rachelhaub@gmail.com>
2020-12-01 13:08:06 -0800

    Added menu functionality for prototyping

---
+++ Alone-Not-Lonely/Assets/Animations/ShakeEvent.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeEvent : MonoBehaviour
+{
+    public Animator[] animationList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void Shake()
+    {
+        foreach(Animator anim in animationList)
+        {
+            anim.SetBool("Play", true);
+        }
+        this.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Shake();
+        }
+    }
+}
--- Alone-Not-Lonely/Assets/Scripts/PlayerAbilityController.cs
+++ Alone-Not-Lonely/Assets/Scripts/PlayerAbilityController.cs
@@ -1,16 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerAbilityController : MonoBehaviour
 {
     bool waitingForInput = false;
     GameObject currentGrab = null;
     bool holdingObj = false;
+    public Text grabText;
+    public Text releaseText;
+
+    private Vector3 spawnPosition;
+    private Quaternion spawnRotation;
+
+    private Vector3 camSpawnPosition;
+    private Quaternion camSpawnRotation;
     // Start is called before the first frame update
     void Start()
     {
-        
+        grabText.gameObject.SetActive(false);
+        releaseText.gameObject.SetActive(false);
+
+        spawnPosition = transform.position;
+        spawnRotation = transform.rotation;
+
+        camSpawnPosition = Camera.main.gameObject.transform.position;
+        camSpawnRotation = Camera.main.gameObject.transform.rotation;
     }
 
     // Update is called once per frame
@@ -19,6 +35,8 @@ public class PlayerAbilityController : MonoBehaviour
         if(waitingForInput && currentGrab != null && Input.GetKeyDown(KeyCode.E))
         {
             Debug.Log("grabbed");
+            grabText.gameObject.SetActive(false);
+            releaseText.gameObject.SetActive(true);
             currentGrab.gameObject.transform.parent = this.transform;
             //currentGrab.gameObject.GetComponent<Rigidbody>().freezeRotation = true;
             currentGrab.GetComponent<SphereCollider>().enabled = false;
@@ -27,6 +45,7 @@ public class PlayerAbilityController : MonoBehaviour
         }
         else if(holdingObj && Input.GetKeyDown(KeyCode.E))
         {
+            grabText.gameObject.SetActive(true);
             Debug.Log("dropped");
             currentGrab.gameObject.transform.parent = null;
             //currentGrab.gameObject.GetComponent<Rigidbody>().freezeRotation = false;
@@ -39,9 +58,28 @@ public class PlayerAbilityController : MonoBehaviour
     {
         if(collision.gameObject.CompareTag("Grabable"))
         {
+            grabText.gameObject.SetActive(true);
+            releaseText.gameObject.SetActive(false);
             currentGrab = collision.gameObject;
             waitingForInput = true;
             Debug.Log("waiting for input");
+        }
+        if(collision.gameObject.CompareTag("Deadly"))
+        {
+            this.gameObject.SetActive(false);
+            transform.position = spawnPosition;
+            transform.rotation = spawnRotation;
+            Camera.main.gameObject.transform.position = camSpawnPosition;
+            Camera.main.gameObject.transform.rotation = camSpawnRotation;
+            this.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider collision) 
+    {
+        if(collision.gameObject.CompareTag("Grabable"))
+        {
+            grabText.gameObject.SetActive(false);
         }
     }
 }
