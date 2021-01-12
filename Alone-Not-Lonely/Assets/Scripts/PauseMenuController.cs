ea4aa57c60856981675b62b391d7e525614cb318
rbaua <rachelhaub@gmail.com>
2021-01-11 18:55:05 -0800

    Version change + beginning of pause menu implementation

---
+++ Alone-Not-Lonely/Assets/Scripts/PauseMenuController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool gamePaused;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) && gamePaused == false)
+        {
+            Time.timeScale = 0;
+            gamePaused = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && gamePaused == true)
+        {
+            Time.timeScale = 1;
+            gamePaused = false;
+        }
+
+        if(gamePaused)
+        {
+            UpdatePauseUI();
+        }
+    }
+
+    void UpdatePauseUI()
+    {
+        
+    }
+}
