fe0332a814c895e6184fcb8e057baeda392d0656
rbaua <rachelhaub@gmail.com>
2021-01-18 13:44:25 -0800

    Basic patrol points AI

---
+++ Alone-Not-Lonely/Assets/Scripts/PatrolPointsController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolPointsController : MonoBehaviour
+{
+    public Transform[] patrolPoints;
+    public float speed = 5f;
+    private int currentGoal;
+
+    private enum State {Waiting, Moving};
+    private State currentState;
+
+    private float waitTime = 3f;
+    private float currentWaitTime = 0;
+
+    private float colliderCooldown = 2f;
+    private float currentColliderCooldown = 0f;
+    private bool inColliderCooldown = false;
+
+    private Vector3 lastTransform;
+    private bool stuck = false;
+    private float stuckTimer;
+    public float stuckTimeToMove = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentGoal = 0;
+        currentState = State.Moving;
+        lastTransform = this.transform.position;
+    }
+
+    void FixedUpdate() {
+        if(currentState == State.Moving && Vector3.Distance(transform.position, patrolPoints[currentGoal].position) > .1f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, patrolPoints[currentGoal].position, .01f * speed);
+        }
+        else if(currentState == State.Moving && Vector3.Distance(transform.position, patrolPoints[currentGoal].position) <= .1f)
+        {
+            currentState = State.Waiting;
+            currentWaitTime += Time.deltaTime;
+        }
+        float movementLastFrame = Vector3.Distance(this.transform.position, lastTransform);
+        if(currentState == State.Moving && movementLastFrame < .099999f)
+        {
+            stuck = true;
+        }
+        else if(currentState == State.Moving && movementLastFrame >= .099999f)
+        {
+            stuck = false;
+            stuckTimer = 0f;
+        }
+
+        if(stuck && stuckTimer < stuckTimeToMove)
+        {
+            stuckTimer += Time.deltaTime;
+        }
+        else if(stuck && stuckTimer >= stuckTimeToMove)
+        {
+            currentGoal++;
+            if(currentGoal >= patrolPoints.Length)
+            {
+                currentGoal = currentGoal % patrolPoints.Length;
+            }
+        }
+
+        if(currentState == State.Waiting && currentWaitTime < waitTime)
+        {
+            currentWaitTime += Time.deltaTime;
+        }
+        else if(currentState == State.Waiting && currentWaitTime >= waitTime)
+        {
+            currentState = State.Moving;
+            currentWaitTime = 0f;
+            currentGoal++;
+            if(currentGoal >= patrolPoints.Length)
+            {
+                currentGoal = currentGoal % patrolPoints.Length;
+            }
+        }
+
+        if(inColliderCooldown && currentColliderCooldown < colliderCooldown)
+        {
+            currentColliderCooldown += Time.deltaTime;
+        }
+        else if(inColliderCooldown && currentColliderCooldown >= colliderCooldown)
+        {
+            this.GetComponent<Collider>().enabled = true;
+            currentColliderCooldown = 0f;
+        }
+        lastTransform = this.transform.position;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Portal"))
+        {
+            this.transform.position = other.GetComponent<PortalController>().partnerPortal.transform.position;
+            inColliderCooldown = true;
+            this.GetComponent<Collider>().enabled = false;
+        }
+    }
+}
