df7845bd99a36f33d4a4ddf6234bb25ee42997f0
rbaua <rachelhaub@gmail.com>
2021-01-25 18:41:36 -0800

    set up a basic playtest end

---
+++ Alone-Not-Lonely/Assets/Scripts/AtticLevelEndController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AtticLevelEndController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator ballFallingAnim;
+    public Text interactionUI;
+
+    private bool ableToInteract;
+    private Collider interactionCollider;
+
+    void Start()
+    {
+        interactionUI.gameObject.SetActive(false);
+        ableToInteract = false;
+        interactionCollider = GetComponent<SphereCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ableToInteract && Input.GetKeyDown(KeyCode.E))
+        {
+            interactionCollider.enabled = false;
+            //trigger animation
+            //fancy camera movements? VFX? all fair game
+            //and then transition into next level (or transitional animation)
+            interactionUI.gameObject.SetActive(false);
+            SceneManager.LoadScene("EndPlaytest");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            ableToInteract = true;
+            interactionUI.gameObject.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other) 
+    {
+        if(other.CompareTag("Player"))
+        {
+            ableToInteract = false;
+            interactionUI.gameObject.SetActive(false);
+        }
+    }
+}
