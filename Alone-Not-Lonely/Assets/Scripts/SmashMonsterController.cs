e7741aacf237d9e24076231d8d4fe849fde841b9
rbaua <rachelhaub@gmail.com>
2021-03-27 17:28:32 -0700

    Basic movement for smash monster, no smashing occurs (yet)

---
+++ Alone-Not-Lonely/Assets/Scripts/SmashMonsterController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmashMonsterController : MonoBehaviour
+{
+    public float timeToSmash = 5f;
+    public float smashCountdown = 0;
+    private bool smashing = false;
+    private Vector3 groundLocation;
+    private Vector3 raisedLocation;
+    private Rigidbody rb;
+    public float liftHeight = 5f;
+    public float deltaPos = .25f;
+
+    private void Start() {
+        groundLocation = this.transform.position;
+        raisedLocation = this.transform.position;
+        raisedLocation += new Vector3(0, liftHeight, 0);
+        rb = this.GetComponent<Rigidbody>();
+        StartCoroutine("ReturnToPeak");
+    }
+
+    private void Update() {
+        if(!smashing && smashCountdown < timeToSmash)
+        {
+            smashCountdown += Time.deltaTime;
+        }
+        else if(!smashing && smashCountdown >= timeToSmash)
+        {
+            smashing = true;
+            smashCountdown = 0f;
+        }
+    }
+
+    private void FixedUpdate() {
+        if(smashing)
+        {
+            rb.MovePosition(this.transform.position - new Vector3(0,deltaPos * Time.deltaTime, 0));
+            deltaPos += .1f;
+            if(transform.position.y < groundLocation.y)
+            {
+                smashing = false;
+                deltaPos = 1f;
+            }
+        }
+        else
+        {
+            rb.MovePosition(this.transform.position - new Vector3(0,-deltaPos * Time.deltaTime, 0));
+        }
+    }
+}
