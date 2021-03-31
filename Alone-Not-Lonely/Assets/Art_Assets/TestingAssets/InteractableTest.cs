c684e278cc6c11e825300ae504439f4e483f8c7b
rbaua <rachelhaub@gmail.com>
2021-03-31 14:14:20 -0700

    Added animation/interaction mode controller

---
+++ Alone-Not-Lonely/Assets/Art_Assets/TestingAssets/InteractableTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableTest : Interactable
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        base.playerRef = (Player)FindObjectOfType(typeof(Player));
+        base.playerRef._actionMap.Platforming.InteractionTest.performed += interact => PlayerInteract();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
