2aedda79ed4c37b0920a292a0fa5f21dfcc9fa18
rbaua <rachelhaub@gmail.com>
2021-04-13 13:36:48 -0700

    Added an audio fade in/out (softens the edges, not super noticeable though)

--- Alone-Not-Lonely/Assets/Scripts/RangedAudioController.cs
+++ Alone-Not-Lonely/Assets/Scripts/RangedAudioController.cs
@@ -8,6 +8,10 @@ public class RangedAudioController : MonoBehaviour
     public Transform target;
     private bool audioPlaying;
     public float range;
+
+    private bool fadingIn = false;
+    private bool fadingOut = false;
+    public float fadeSpeed = .01f;
     void Start()
     {
         sourceToPlay = GetComponent<AudioSource>();
@@ -19,14 +23,45 @@ public class RangedAudioController : MonoBehaviour
     {
         float dist = Vector3.Distance(this.transform.position, target.position);
         if(!audioPlaying && dist < range){
-            Debug.Log("Play Growl");
+            sourceToPlay.volume = 0;
             sourceToPlay.Play();
+            StartCoroutine(FadeIn());
             audioPlaying = true;
         }
         else if(audioPlaying && dist >=range){
-            Debug.Log("Pause Growl");
+            StartCoroutine(FadeOut());
+        }
+        if(audioPlaying && fadingOut && sourceToPlay.volume <=0)
+        {
+            StopCoroutine(FadeOut());
+            fadingOut = false;
             audioPlaying = false;
             sourceToPlay.Pause();
+        }
+        if(audioPlaying && fadingIn && sourceToPlay.volume >= 1)
+        {
+            StopCoroutine(FadeIn());
+            fadingIn = false;
+        }
+    }
+
+    IEnumerator FadeIn()
+    {
+        while(sourceToPlay.volume < 1)
+        {
+            fadingIn = true;
+            sourceToPlay.volume += fadeSpeed;
+            yield return null;
+        }
+    }
+
+    IEnumerator FadeOut()
+    {
+        while(sourceToPlay.volume > 0)
+        {
+            fadingOut = true;
+            sourceToPlay.volume -= fadeSpeed;
+            yield return null;
         }
     }
 }
