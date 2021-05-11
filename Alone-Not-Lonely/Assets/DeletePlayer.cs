f34dcd0ab63553ea68e8323f7764b148720b1d34
rbaua <rachelhaub@gmail.com>
2021-05-10 21:00:49 -0700

    Last minute build fixes

---
+++ Alone-Not-Lonely/Assets/DeletePlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeletePlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Player _player = FindObjectOfType<Player>();
+        Destroy(Player.instance);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Alone-Not-Lonely/Assets/ReturnToKitchenScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToKitchenScript : ContextualUI
+{
+    public Vector3 positionToReturnTo;
+    private Player _player;
+
+    private bool canGoToAttic;
+
+    void Start()
+    {
+        base.Start();
+        _player = FindObjectOfType<Player>();
+        positionToReturnTo = new Vector3(32.7400017f,4.98999977f,-62.7200012f);
+    }
+    
+    /// <summary>
+    /// OnTriggerEnter is called when the Collider other enters the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            base.OnTriggerEnter(other);
+            canGoToAttic = true;
+            _player._actionMap.Platforming.ReturnToLevel.performed += interact => GoToAttic();
+        }
+    }
+
+    void OnTriggerExit(Collider other) 
+    {
+        if(other.CompareTag("Player"))
+        {
+            base.OnTriggerExit(other);
+            canGoToAttic = false;
+            _player._actionMap.Platforming.ReturnToLevel.performed -= interact => GoToAttic();
+        }
+    }
+
+    void GoToAttic()
+    {
+        if(canGoToAttic)
+        {
+            _player.gameObject.SetActive(false);
+            _player.gameObject.transform.position =  positionToReturnTo;
+            _player.gameObject.SetActive(true);
+            SceneManager.LoadScene("Kitchen");
+            canGoToAttic = false;
+            _player._actionMap.Platforming.ReturnToLevel.performed -= interact => GoToAttic();
+        }
+    }
+}
