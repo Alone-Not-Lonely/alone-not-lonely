9713b4e5aaab0365bf10bbafb3d26c73c025033b
rbaua <rachelhaub@gmail.com>
2021-05-24 11:55:50 -0700

    Last minute build fixes

--- Alone-Not-Lonely/Assets/LevelEndTrigger.cs
+++ Alone-Not-Lonely/Assets/LevelEndTrigger.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class LevelEndTrigger : MonoBehaviour
 {
     public GameObject levelParent;
-    
+    public float dissolveStep = .01f;
     private void Awake() {
 
     }
@@ -24,17 +24,17 @@ public class LevelEndTrigger : MonoBehaviour
     {
         if(levelParent.name != "Attic2" && other.CompareTag("Player"))
         {
-            CheckObjects(levelParent);
+            CheckObjectsFirst(levelParent);
         }
         else if(levelParent.name == "Attic2" && other.CompareTag("Grabable"))//check when chest is on door
         {
-            CheckObjects(levelParent);
+            CheckObjectsFirst(levelParent);
         }
     }
 
     void CheckObjects(GameObject curr)
     {
-        ProgressionTracker.instance.MarkSceneCompleted(levelParent.name);
+        //ProgressionTracker.instance.MarkSceneCompleted(levelParent.name);
         foreach(Transform child in curr.transform)
         {
             if(child.gameObject.CompareTag("Monster") ||
@@ -46,6 +46,7 @@ public class LevelEndTrigger : MonoBehaviour
             {
                 child.gameObject.SetActive(false);
             }
+
             //else if(child.gameObject.GetComponent<Item>() != null && 
                //child.gameObject.GetComponent<Item>().key.Contains("Key"))
             //{
@@ -53,5 +54,54 @@ public class LevelEndTrigger : MonoBehaviour
             //}
             CheckObjects(child.gameObject);
         }
+    }
+
+    void CheckObjectsFirst(GameObject curr)
+    {
+        ProgressionTracker.instance.MarkSceneCompleted(levelParent.name);
+        foreach(Transform child in curr.transform)
+        {
+            if(child.gameObject.CompareTag("RemoveOnStart") ||
+               child.gameObject.CompareTag("Deadly"))
+            {
+                child.gameObject.SetActive(false);
+            }
+            else if(child.gameObject.CompareTag("Monster") || 
+                child.gameObject.GetComponent<PatrolPointsController>()!=null ||
+               child.gameObject.GetComponent<PortalController>() != null ||
+               child.gameObject.GetComponent<ElevatorMonsterController>() != null)
+            {
+                if(child.gameObject.GetComponentInChildren<SpriteRenderer>() != null)
+                {
+                    StartCoroutine(DissolveSprite(child.gameObject.GetComponentInChildren<SpriteRenderer>()));
+                }
+                else if(curr.GetComponent<SpriteRenderer>() != null)
+                {
+                    StartCoroutine(DissolveSprite(curr.GetComponent<SpriteRenderer>()));
+                }
+            }
+            //else if(child.gameObject.GetComponent<Item>() != null && 
+               //child.gameObject.GetComponent<Item>().key.Contains("Key"))
+            //{
+                //child.gameObject.SetActive(false);
+            //}
+            CheckObjectsFirst(child.gameObject);
+        }
+    }
+
+    IEnumerator DissolveSprite(SpriteRenderer thisSprite)
+    {
+        while(thisSprite.material.GetFloat("_Dissolve") > 0)
+        {
+            thisSprite.material.SetFloat("_Dissolve", thisSprite.material.GetFloat("_Dissolve") - dissolveStep);
+            yield return null;
+        }
+        if(thisSprite.gameObject.transform.parent.gameObject.GetComponent<Billboard>() == null)
+        {
+            thisSprite.gameObject.SetActive(false);
+        }
+        else
+            thisSprite.gameObject.transform.parent.gameObject.SetActive(false);
+        yield return null;
     }
 }
