fade08696d82e4947f0f9ceab504c955658efd7c
“Adrian <adrmille@ucsc.edu>
2021-05-28 07:40:46 -0700

    climbchecker reverted.

--- Alone-Not-Lonely/Assets/Scripts/PlayerScripts/ClimbChecker.cs
+++ Alone-Not-Lonely/Assets/Scripts/PlayerScripts/ClimbChecker.cs
@@ -21,19 +21,6 @@ public class ClimbChecker : MonoBehaviour
     private PlayerMovementController pMC;
     public GameObject climbableObject;
 
-    //Dealing with hand model swaps
-    /*
-    protected MeshFilter rHandMesh;
-    protected MeshFilter lhandMesh;
-
-    public Mesh relaxedRHand;
-    public Mesh relaxedLHand;
-    public Mesh grabRHand;
-    public Mesh grabLHand;
-    public Mesh climbRHand;
-    public Mesh climbLHand;
-    */
-
     //Scripts using this will check if Vector3.zero
     //So lets just be sure not to have any climbable places at origin
 
@@ -48,16 +35,13 @@ public class ClimbChecker : MonoBehaviour
         playerHeight = GetComponentInParent<CharacterController>().height;
         playerRadius = GetComponentInParent<CharacterController>().radius;
         pMC = GetComponentInParent<PlayerMovementController>();
-        //rHandMesh = rhand.GetComponentInChildren<MeshFilter>();
-        //lhandMesh = lhand.GetComponentInChildren<MeshFilter>();
-        clear();
+        Debug.Log("Player Radius: " + playerRadius);
     }
 
     private void FixedUpdate()
     {
         if (pControl.isGrounded) { adjustHeight(); }
         updateHands();
-
     }
 
     public void clear()
@@ -71,7 +55,7 @@ public class ClimbChecker : MonoBehaviour
         }
     }
 
-    //should be a coroutine for performance, but testing the idea first.
+    //should be a coroutine for performance, but testing the idea first. 
     public void adjustHeight()
     {
         //Checks if player is close to object
@@ -80,10 +64,21 @@ public class ClimbChecker : MonoBehaviour
 
         Debug.DrawRay(transform.position, (transform.forward * reachDepth), Color.blue);
         //If an object is close enough to gabe to climb:
-        if (Physics.SphereCast(obNearRay,detectRadius, out obNearHit, reachDepth, ~ignoreLayer) && (obNearHit.collider.isTrigger == false))
+        if (Physics.SphereCast(obNearRay, detectRadius, out obNearHit, reachDepth, ~ignoreLayer))
         {
-            //Debug.Log("Hit something");
-            //Object Gabe could concievably climb
+            //ignore triggers
+            if (obNearHit.collider.isTrigger)
+            {
+                clear();
+                return;
+            }
+
+            //Don't climb on the monsters, dear
+            if (obNearHit.transform.tag == "Monster")
+            {
+                clear();
+                return;
+            }
 
             RaycastHit canLandHit;
             Vector3 landingRayStart = (transform.position + maxClimbHeight * Vector3.up);
@@ -91,16 +86,41 @@ public class ClimbChecker : MonoBehaviour
             Debug.DrawRay(landingRay.origin, landingRay.direction, Color.yellow);
 
             //If the landing is short enough for gabe to climb:
-            if (!Physics.Raycast(landingRay, out canLandHit, maxDepth, ~ignoreLayer) || (canLandHit.collider.isTrigger == true))
-            {//We are close enough and HAVE found the object's top
+            if (!Physics.Raycast(landingRay, out canLandHit, maxDepth, ~ignoreLayer) || (canLandHit.collider.isTrigger))
+            {//We are close enough and HAVE found the object's top 
                 Vector3 canStandRayStart = landingRayStart + (landingRay.direction * maxDepth);
                 Ray canStandRay = new Ray(canStandRayStart, -transform.up);
                 Debug.DrawRay(canStandRay.origin, canStandRay.direction, Color.green);
 
-                RaycastHit canStandHit;
+                RaycastHit canStandHit = obNearHit;//DIDN'T THINK ABOUT THIS MUCH, MAY CAUSE BUG
+                RaycastHit[] canStandHits = Physics.RaycastAll(canStandRay, Mathf.Infinity, ~ignoreLayer);
                 //Debug.Log((!Physics.Raycast(landingRay, out canLandHit, maxDepth)) + " and " + (reachHeight != 0));
-                if (Physics.Raycast(canStandRay, out canStandHit, Mathf.Infinity, ~ignoreLayer) && (canStandHit.collider.isTrigger == false))
+                //if (Physics.Raycast(canStandRay, out canStandHit, Mathf.Infinity, ~ignoreLayer) && (canStandHit.collider.isTrigger == false))
+                float tempMax = float.MinValue;
+                bool verified = false;
+
+                foreach (RaycastHit hit in canStandHits)
                 {
+                    //Finds the highest object on a possible stack
+                    //don't need to worry about too high b/c ray fires from max height->downward
+                    if (hit.point.y > tempMax && !hit.collider.isTrigger)
+                    {
+                        canStandHit = hit;
+                        tempMax = canStandHit.point.y;
+                    }
+
+                    //Checks to make sure the thing we originally wanted to climb is amungst a possible stack of things
+                    if (hit.transform == obNearHit.transform && !hit.collider.isTrigger)
+                    {
+                        //Debug.Log("verified by " + hit.transform.name);
+                        verified = true;
+                    }
+                }
+
+                if (verified)
+                {
+                    //Debug.Log("highestObject = " + canStandHit.transform.name);
+
                     //Debug.Log("Can stand on: " + canStandHit.collider.name);
                     //Debug.Log("Triggered by contact with: " + obNearHit.collider.name);
                     Vector3 possEdge = new Vector3(obNearHit.point.x, canStandHit.point.y, obNearHit.point.z);
@@ -108,18 +128,22 @@ public class ClimbChecker : MonoBehaviour
                     Vector3 probClimbPoint = new Vector3(canStandHit.point.x, edge.y + (playerHeight * 1.1f), canStandHit.point.z);
                     //Checks to see if the intended landing point is higher than the origin of the object that gabe first ran up against
                     //Possible flaws: If origin is somehow above the object (unlikely but possible)
-                    if (possEdge.y > obNearHit.point.y)
-                    {
-                        //Debug.Log("Distances: edge y " + possEdge.y + ", ob y " + obNearHit.collider.transform.position.y);
-                        climbableObject = canStandHit.transform.gameObject;
-                        edge = possEdge;
-                        climbablePoint = probClimbPoint;
-                    }
-                    else
-                    {
-                        clear();
-                    }
+                    //if (possEdge.y + .4f > obNearHit.point.y)
+                    //{
+                    //Debug.Log("Distances: edge y " + possEdge.y + ", ob y " + obNearHit.collider.transform.position.y);
+                    climbableObject = canStandHit.transform.gameObject;
+                    edge = possEdge;
+                    climbablePoint = probClimbPoint;
                 }
+                else
+                {
+                    clear();
+                }
+                //}
+            }
+            else
+            {
+                clear();
             }
         }
         else
@@ -132,21 +156,18 @@ public class ClimbChecker : MonoBehaviour
 
     private void updateHands()
     {
-        
         Vector3 goToPoint = (transform.position + (Vector3.up * handRestingHeight));
-        /*
+
         if (edge != transform.position)
         {
             goToPoint = edge;
         }
-        */
-        //Debug.Log("Go to point: "+goToPoint);
 
-        //rhand.position = Vector3.Lerp(rhand.position, goToPoint + (transform.right * handOffset), handMoveSpeed);
-        //if (!(pAbil.currentGrab != null && pAbil.heldObject && pAbil.heldObject.gameObject.GetComponent<SquashedObject>() != null))
-        //{
-            //lhand.position = Vector3.Lerp(lhand.position, goToPoint - (transform.right * handOffset), handMoveSpeed);
-        //}
+        rhand.position = Vector3.Lerp(rhand.position, goToPoint + (transform.right * handOffset), handMoveSpeed);
+        if (!(pAbil.currentGrab != null && pAbil.heldObject && pAbil.heldObject.gameObject.GetComponent<SquashedObject>() != null))
+        {
+            lhand.position = Vector3.Lerp(lhand.position, goToPoint - (transform.right * handOffset), handMoveSpeed);
+        }
         /*
         if (Vector3.Distance(rhand.position, edge) < .1)
         {
--- Alone-Not-Lonely/Assets/handBehavior.cs
+++ Alone-Not-Lonely/Assets/handBehavior.cs
@@ -16,6 +16,9 @@ public class handBehavior : MonoBehaviour
     private int hQ;
     private PlayerAbilityController pAbil;
     private ClimbChecker cCheck;
+    private GameObject focus;
+    private RaycastHit mostRecentHit;
+    //public float handOffset = .3f; for later
     // Start is called before the first frame update
     void Start()
     {
@@ -30,23 +33,60 @@ public class handBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("current hand mesh: "+handMesh.mesh);           
+        //Debug.Log("current hand mesh: "+handMesh.mesh);           
         if(cCheck.climbablePoint != Vector3.zero && handMesh.mesh!=climbHand)
         {
             //climbing hands
-            //handMesh.mesh = climbHand;
+            handMesh.mesh = climbHand;
+            Debug.Log("Climb Hands Selected");
         }
         else if(pAbil.holdingObject && handMesh.mesh != grabHand)
         {
             //grab hands
-            //handMesh.mesh = grabHand;
+            handMesh.mesh = grabHand;
+            Debug.Log("Grab Hands Selected");
         }
         else
         {
             //resting hands
-            //handMesh.mesh = relaxedHand;
+            handMesh.mesh = relaxedHand;
+            Debug.Log("Relaxed Hand Selected");
         }
         
         //transform.Rotate(Vector3.up * hQ, Space.Self);
     }
+
+    public void recieveNearHit(RaycastHit hit)
+    {
+        //Debug.Log("Recieving hit");
+        focus = hit.transform.gameObject;
+        mostRecentHit = hit;
+    }
+
+    private void LateUpdate()
+    {
+        
+    }
 }
+
+/* private void updateHands()
+ {
+
+     Vector3 goToPoint = (transform.position + (Vector3.up * handRestingHeight));
+
+     if (edge != transform.position)
+     {
+         goToPoint = edge;
+     }
+     //Debug.Log("Go to point: "+goToPoint);
+
+     //rhand.position = Vector3.Lerp(rhand.position, goToPoint + (transform.right * handOffset), handMoveSpeed);
+     //if (!(pAbil.currentGrab != null && pAbil.heldObject && pAbil.heldObject.gameObject.GetComponent<SquashedObject>() != null))
+     //{
+         //lhand.position = Vector3.Lerp(lhand.position, goToPoint - (transform.right * handOffset), handMoveSpeed);
+     //}
+     if (Vector3.Distance(rhand.position, edge) < .1)
+     {
+         //hand closed indicator
+     }
+ }*/
