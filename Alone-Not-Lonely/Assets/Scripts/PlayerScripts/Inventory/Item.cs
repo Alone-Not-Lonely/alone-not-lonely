467e96f63aa4a09086fc3537e1a5bb59d01ad991
rbaua <rachelhaub@gmail.com>
2021-05-31 19:00:12 -0700

    fixed key spamming

--- Alone-Not-Lonely/Assets/Scripts/PlayerScripts/Inventory/Item.cs
+++ Alone-Not-Lonely/Assets/Scripts/PlayerScripts/Inventory/Item.cs
@@ -15,6 +15,8 @@ public class Item : MonoBehaviour
 
     private AudioSource itemPickup;
 
+    bool itemGet = false;
+
     void Start()
     {
         bool destroyThis = false;
@@ -45,14 +47,14 @@ public class Item : MonoBehaviour
         {
             return;
         }
-        if(!inventory.items.Contains(this.name))
+        if(!itemGet)
         {
+            inventory.addItem(this);
+            itemGet = true;
             Deactivate();//cannot destroy, causes scripting complications
             doorCU.nextPrompt();//advance prompt on pickup
             doorCU.proController.clearPrompters();//precaution against item disappearing
-            //could have some kind of ienumerator that waits a second with a "wow you found a key" prompt
-        
-            inventory.addItem(this);
+            //could have some kind of ienumerator that waits a second with a "wow you found a key" promp
             //itemPickup.Play();
         }
     }
